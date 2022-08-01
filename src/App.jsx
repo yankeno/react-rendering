919d9f96ae88acaddba4f524db7ce14417a99a3a
yankeno <okinok@MacBookPro.local>
2022-08-01 23:39:33 +0900

    一旦コミット

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { ChildArea } from "./components/ChildArea";
 import "./style.css";
 
@@ -14,13 +14,25 @@ export function App() {
     setText(e.target.value);
   }
 
+  /**
+   * アロー関数はレンダリングが走るたびに再生成される
+   * -> 前回レンダリングとは違うものとして扱われるため、
+   *    関数を渡している 子component も再レンダリングが走る
+   * -> アロー関数を useCallBack() で囲むことで、第二引数に指定した値が更新された場合のみ
+   *    子component の再レンダリングを実行させることができる
+   * -> 第二引数を空配列にすることで関数の再生成による 子component の再レンダリングを無効化できる 
+  */
+  const onClickClose = useCallback(() => {
+    setOpen(false);
+  }, []);
+
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   )
 }
